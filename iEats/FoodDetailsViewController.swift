0b21ff984a3ffd3724d06cf78129c3946aae532b
Matheus <matheus.castelo@gmail.com>
2019-03-27 14:46:36 -0300

    Adding apple pay sheet to project

--- iEats/FoodDetailsViewController.swift
+++ iEats/FoodDetailsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import PassKit
 
-class FoodDetailsViewController: UIViewController {
+class FoodDetailsViewController: UIViewController, PKPaymentAuthorizationViewControllerDelegate {
     
     @IBOutlet weak var foodImageView: UIImageView!
     @IBOutlet weak var foodNameLabel: UILabel!
@@ -26,7 +26,6 @@ class FoodDetailsViewController: UIViewController {
     }
     
     func setupForFood(){
-        
         foodNameLabel.text = selectedFood?.name
         foodPriceLabel.text = String(format: "R$ %.2f", selectedFood?.price ?? 10000.00)
         foodDescriptionLabel.text = selectedFood?.detailedDescription
@@ -52,10 +51,53 @@ class FoodDetailsViewController: UIViewController {
     func addApplePayButton(){
         let applePayButton = PKPaymentButton(paymentButtonType: .buy, paymentButtonStyle: .black)
         applePayButton.translatesAutoresizingMaskIntoConstraints = false
+        applePayButton.addTarget(self, action: #selector(applePayButtonTapped(sender:)), for: .touchUpInside)
         view.addSubview(applePayButton)
         
         view.addConstraint(NSLayoutConstraint(item: applePayButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: applePayButton, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0))
         view.addConstraint(NSLayoutConstraint(item: applePayButton, attribute: .width, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .width, multiplier: 0.8, constant: 0))
+    }
+    
+    @objc private func applePayButtonTapped(sender: UIButton){
+        let request = PKPaymentRequest()
+        let SupportedPaymentNetworks = [PKPaymentNetwork.masterCard, PKPaymentNetwork.visa, PKPaymentNetwork.amex]
+        request.merchantIdentifier = "merchant.com.shiningdevelopers"
+        request.supportedNetworks = SupportedPaymentNetworks
+        request.merchantCapabilities = PKMerchantCapability.capability3DS
+        request.countryCode = "BR"
+        request.currencyCode = "BRL"
+        request.requiredShippingContactFields = [.name, .postalAddress]
+        
+        let delivery = PKShippingMethod(label: "Delivery", amount: NSDecimalNumber(integerLiteral: 5), type: .final)
+        delivery.identifier = "deliveryShipping"
+        delivery.detail = "We'll deliver it right at your door"
+        
+        let inStore = PKShippingMethod(label: "In Store", amount: NSDecimalNumber(integerLiteral: 0), type: .final)
+        inStore.identifier = "inStoreShipping"
+        inStore.detail = "You'll come visit us!"
+        
+        request.shippingMethods = [delivery, inStore]
+        
+        let foodItem = PKPaymentSummaryItem(label: selectedFood!.name, amount: NSDecimalNumber(floatLiteral: Double(selectedFood!.price)))
+        let shipingItem = PKPaymentSummaryItem(label: "shipping", amount: NSDecimalNumber(floatLiteral: 5.00))
+        let total = PKPaymentSummaryItem(label: "iEats", amount: foodItem.amount.adding(shipingItem.amount))
+        
+        request.paymentSummaryItems = [ foodItem, shipingItem, total]
+        let applePayController = PKPaymentAuthorizationViewController(paymentRequest: request)
+        if let viewController = applePayController {
+            viewController.delegate = self
+            present(viewController, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK - PKPaymentAuthorizationViewControllerDelegate
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        //Perform Segue
+    }
+    
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didSelect shippingMethod: PKShippingMethod, handler completion: @escaping (PKPaymentRequestShippingMethodUpdate) -> Void) {
+        <#code#>
     }
 }
